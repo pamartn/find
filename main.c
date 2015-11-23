a67924495ee28b51235a8c21eba6cf3bb29db69f
Nicolas Pamart <nicolaspmt@gmail.com>
2015-11-23 16:58:09 +0100

    finished the command :)

--- main.c
+++ main.c
@@ -49,10 +49,10 @@ int type_correspond(const struct stat *buf, char type){
 }
 
 int executable(const struct stat *buf){
-        return (buf->st_mode & S_IRWXU) == S_IXUSR || (buf->st_mode & S_IRWXG) == S_IXGRP || (buf->st_mode & S_IRWXO) == S_IXOTH;
+        return ((buf->st_mode & S_IXUSR) == S_IXUSR) || ((buf->st_mode & S_IXGRP) == S_IXGRP) || ((buf->st_mode & S_IXOTH) == S_IXOTH);
 }
 int nom_correspond(const char *chemin, const char *motif){
-        return strcmp(strrchr(chemin,'/')+1,motif) == 0;
+	return strcmp(strrchr(chemin,'/')+1,motif) == 0;
 }
 
 int vide(const struct stat *buf){
@@ -61,12 +61,12 @@ int vide(const struct stat *buf){
 int plus_recent(const struct stat *buf, const char *fichier_reference){
         struct stat buf2;
         stat(fichier_reference, &buf2);
-        return buf2.st_mtim.tv_nsec < buf->st_mtim.tv_nsec;
+        return buf2.st_mtim.tv_sec < buf->st_mtim.tv_sec;
 }
 
 void traiter_fichier(const char* chemin, const expression *exp, int suivre_lien){
 	struct stat file;
-	suivre_lien == 1 ? lstat(chemin, &file) : stat(chemin, &file);
+	suivre_lien ? stat(chemin, &file) : lstat(chemin, &file);
 	switch(exp->type){
 		case NAME:
 			if(nom_correspond(chemin,exp->operande.motif))
@@ -85,7 +85,7 @@ void traiter_fichier(const char* chemin, const expression *exp, int suivre_lien)
 				printf("%s\n", chemin);
 		break;
 		case NEWER:
-			if(plus_recent(&file, chemin))
+			if(plus_recent(&file, exp->operande.chemin))
 				printf("%s\n", chemin);		
 		break;
 	}		
@@ -93,18 +93,25 @@ void traiter_fichier(const char* chemin, const expression *exp, int suivre_lien)
 
 void parcourir_repertoire(const char* chemin, const expression *exp, int suivre_lien){
 	DIR *dir = opendir(chemin);
-	struct dirent *entry = readdir(dir);
-	while(entry != NULL){
+	struct dirent *entry;
+	struct stat dir_stat;
+	if (! dir)
+	{
+		perror(chemin);
+		return;
+	}
+	while((entry = readdir(dir)) != NULL){
 		char buff[strlen(chemin)+strlen(entry->d_name)+2];
                 sprintf(buff,"%s/%s",chemin,entry->d_name);
-		if(entry->d_type == DT_DIR && entry->d_name[0] != '.')
-			parcourir_repertoire(buff, exp, suivre_lien);
-		else
-			traiter_fichier(buff, exp, suivre_lien);
-		entry = readdir(dir);
+		suivre_lien ? stat(buff, &dir_stat) : lstat(buff,&dir_stat);
+		if(strcmp(entry->d_name,"..") != 0){
+			if(((dir_stat.st_mode & S_IFMT) == S_IFDIR)  && strcmp(entry->d_name, ".") != 0)
+				parcourir_repertoire(buff, exp, suivre_lien);
+			else
+				traiter_fichier(buff, exp, suivre_lien);
+		}	
 	}
 	closedir(dir);
-	
 }
 
 void erreur(){
@@ -118,6 +125,7 @@ int main(int argc, char **argv){
 		return 1;
 
 	int linkFlag = 0;
+	int definedExpression = 0;
 	int i = 1;
 	char *path;
 	expression exp;	
@@ -126,28 +134,29 @@ int main(int argc, char **argv){
 	if(argv[1][0] == '-'){
 		if(argv[1][1] == 'L')
 			linkFlag = 1;
-		else if (argv[1][1] != 'P')
-			erreur();
 		i++;
 	}
 	path = argv[i];
 	i++;
-	while(i < argc-1){
-		if(argv[i][0] == '-'){
-			if(strcmp(argv[i]+1, "name")){
-				exp.type = NAME;
-			} else if(strcmp(argv[i]+1, "type"i){                             
+	if(i == argc)
+		erreur();
+	while(i < argc){
+		if(argv[i][0] == '-' && !definedExpression){
+			if(strcmp(argv[i]+1, "name") == 0)
+				exp.type = NAME;		
+			else if(strcmp(argv[i]+1, "type") == 0)                             
                          	exp.type = TYPE;
-                        } else if(strcmp(argv[i]+1, "executable")){                             
-                         	exp.type = exp.EXEC;
-                        } else if(strcmp(argv[i]+1, "empty")){                             
+                        else if(strcmp(argv[i]+1, "executable") == 0)                             
+                         	exp.type = EXEC;
+                        else if(strcmp(argv[i]+1, "empty") == 0)                             
                          	exp.type = EMPTY;
-                        } else if(strcmp(argv[i]+1, "anewer")){                             
+                        else if(strcmp(argv[i]+1, "anewer") == 0)                             
                          	exp.type = NEWER;
-			} else {
+			else
 				erreur();
-			}			
-		} else if (exp.type != NULL){
+			
+			definedExpression = 1;
+		} else if (definedExpression){
 			//Handle arguments
 			switch(exp.type){
                 		case NAME:
@@ -167,7 +176,7 @@ int main(int argc, char **argv){
 			erreur();
 		}
 		i++;
-	
+	}	
 	parcourir_repertoire(path, &exp, linkFlag);
 
 	return 0;
